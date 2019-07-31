4fbb8d47d69e13ab86e22086fdf48893c6737e3c
Joseph Abell <joseph.abell@piksel.com>
2019-07-31 16:56:24 +0100

    remove News and Events title

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,6 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="News and Events" />
         <div css={css`margin-bottom: ${rhythm(2)};`}>
           <h2>Events</h2>
           {events.map(({ node }) => {
