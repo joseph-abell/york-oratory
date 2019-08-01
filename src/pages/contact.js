e4db49cd573ef3437173d72ba171b4a3b9711ed4
Joseph Abell <joseph.abell@piksel.com>
2019-08-01 16:32:40 +0100

    add markdown support on contact, add header data

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@
 import { css, jsx } from '@emotion/core'
 import React from "react"
 import { graphql } from "gatsby"
+import Markdown from 'markdown-to-jsx'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,15 +12,37 @@ class Contact extends React.Component {
     render() {
         const { data } = this.props
         const siteTitle = data.site.siteMetadata.title
-        const contact = data.contact.edges
-        console.log(contact);
+        const { parishPriest, address, telephone, email, directions, visitingPriests } = data.contact.edges[0].node.frontmatter
+
         return (
             <Layout location={this.props.location} title={siteTitle}>
                 <SEO title="Events" />
                 <div css={css`margin-bottom: ${rhythm(2)};`}>
                     <h1>Contact</h1>
-
-
+                    <div>
+                        <h2>Parish Priest:</h2>
+                        <p>{parishPriest}</p>
+                    </div>
+                    <div>
+                        <h2>Address:</h2>
+                        <Markdown css={css`white-space: pre-wrap;`}>{address}</Markdown>
+                    </div>
+                    <div>
+                        <h2>Telephone:</h2>
+                        <p>{telephone}</p>
+                    </div>
+                    <div>
+                        <h2>Email:</h2>
+                        <p>{email}</p>
+                    </div>
+                    <div>
+                        <h2>Directions:</h2>
+                        <Markdown css={css`white-space: pre-wrap;`}>{directions}</Markdown>
+                    </div>
+                    <div>
+                        <h2>Visiting Priests:</h2>
+                        <Markdown css={css`white-space: pre-wrap;`}>{visitingPriests}</Markdown>
+                    </div>
                 </div>
             </Layout>
         )
@@ -35,15 +58,16 @@ export const pageQuery = graphql`
                 title
             }
         }
-        contact: allMarkdownRemark(filter: { frontmatter: { type: { eq: "contact" } } }, sort: { fields: [frontmatter___date], order: DESC }) {
+        contact: allMarkdownRemark(filter: { frontmatter: { title: { eq: "Contact" } } }, sort: { fields: [frontmatter___date], order: DESC }) {
             edges {
                 node {
-                    excerpt
-                    fields {
-                        slug
-                    }
                     frontmatter {
-                        title
+                        parishPriest
+                        address
+                        telephone
+                        email
+                        directions
+                        visitingPriests
                     }
                 }
             }
