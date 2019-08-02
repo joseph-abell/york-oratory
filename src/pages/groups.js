808d9902215305aedd4fc17b4492716da6288b27
Joseph Abell <joseph.abell@piksel.com>
2019-08-02 14:37:16 +0100

    add group description

--- src/pages/groups.js
+++ src/pages/groups.js
@@ -74,6 +74,7 @@ export const pageQuery = graphql`
                     frontmatter {
                         groups {
                             title
+                            description
                             urlSlug
                             body
                             groupType
