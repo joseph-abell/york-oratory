c9ccea8a725f168ef862b5be827cb2af43024960
Joseph Abell <joseph.abell@piksel.com>
2019-08-08 10:03:26 +0100

    policy

---
+++ src/pages/policies.js
@@ -0,0 +1,101 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm } from '../utils/typography';
+
+const PoliciesItem = ({ node, title, primaryImage, policy }) => (
+	<div>
+		<h2 css={css`margin-bottom: ${rhythm(1 / 4)};`}>
+			<a href={policy} style={{ boxShadow: `none` }}>
+				{title}
+			</a>
+		</h2>
+		<small
+			css={css`
+				margin-bottom: ${rhythm(1 / 4)};
+				display: block;
+				color: rgba(0, 0, 0, 0.7);
+			`}
+		>
+			{node.frontmatter.date}
+		</small>
+		<p
+			dangerouslySetInnerHTML={{
+				__html: node.frontmatter.description || node.excerpt
+			}}
+		/>
+		{primaryImage && (
+			<a href={policy}>
+				<img src={primaryImage} alt='' />
+			</a>
+		)}
+	</div>
+);
+
+class Policies extends React.Component {
+	render() {
+		const { data } = this.props;
+		const siteTitle = data.site.siteMetadata.title;
+		const policies = data.policies.edges;
+
+		return (
+			<Layout location={this.props.location} title={siteTitle}>
+				<SEO title='Policies' />
+				<div css={css`margin-bottom: ${rhythm(2)};`}>
+					<h1>Policies</h1>
+					{policies.map(({ node }) => {
+						const title = node.frontmatter.title || node.fields.slug;
+						const primaryImage = node.frontmatter.primaryImage;
+
+						return (
+							<PoliciesItem
+								node={node}
+								key={node.fields.slug}
+								title={title}
+								primaryImage={primaryImage}
+								policy={node.frontmatter.policy}
+							/>
+						);
+					})}
+				</div>
+			</Layout>
+		);
+	}
+}
+
+export default Policies;
+
+export const pageQuery = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		policies: allMarkdownRemark(
+			filter: { frontmatter: { type: { eq: "policies" } } }
+			sort: { fields: [frontmatter___date], order: DESC }
+		) {
+			edges {
+				node {
+					excerpt
+					fields {
+						slug
+					}
+					frontmatter {
+						date(formatString: "MMMM DD, YYYY")
+						title
+						primaryImage
+						description
+						type
+						policy
+					}
+				}
+			}
+		}
+	}
+`;
