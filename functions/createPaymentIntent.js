981cd46eb8769bb0396508d8475510a8a9227c14
Joe <gitkraken@josephabell.co.uk>
2020-05-23 09:55:48 +0100

    fix function

--- functions/createPaymentIntent.js
+++ functions/createPaymentIntent.js
@@ -1,19 +1,19 @@
 // Set your secret key. Remember to switch to your live secret key in production!
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const setupStripe = require('stripe');
-const { STRIPE_KEY } = process.env;
-const stripe = setupStripe(STRIPE_KEY);
+const setupStripe = require("stripe")
+const { STRIPE_KEY } = process.env
+const stripe = setupStripe(STRIPE_KEY)
 
-exports.handler = function async (event, context, callback) {
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1000,
-        currency: 'gbp',
-        // Verify your integration in this guide by including this parameter
-        metadata: {integration_check: 'accept_a_payment'},
-    });
+exports.handler = async (event, context, callback) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: "gbp",
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: "accept_a_payment" },
+  })
 
-    callback(null, {
-        statusCode: 200,
-        body: paymentIntent.client_secret
-    });
+  callback(null, {
+    statusCode: 200,
+    body: paymentIntent.client_secret,
+  })
 }
