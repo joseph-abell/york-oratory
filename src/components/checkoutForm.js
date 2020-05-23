3f12bd8fba1d6fcf2944fdbc3e85765f1f0f47df
Joe <gitkraken@josephabell.co.uk>
2020-05-23 14:44:12 +0100

    move paymentmethod on

--- src/components/checkoutForm.js
+++ src/components/checkoutForm.js
@@ -20,12 +20,43 @@ const CheckoutForm = () => {
         requestPayerEmail: true,
       })
 
+      pr.on("paymentmethod", async ev => {
+        // Confirm the PaymentIntent without handling potential next actions (yet).
+        const { error: confirmError } = await stripe.confirmCardPayment(
+          paymentSecret,
+          { payment_method: ev.paymentMethod.id },
+          { handleActions: false }
+        )
+
+        if (confirmError) {
+          // Report to the browser that the payment failed, prompting it to
+          // re-show the payment interface, or show an error message and close
+          // the payment interface.
+          ev.complete("fail")
+        } else {
+          // Report to the browser that the confirmation was successful, prompting
+          // it to close the browser payment method collection interface.
+          ev.complete("success")
+          // Let Stripe.js handle the rest of the payment flow.
+          const { error, paymentIntent } = await stripe.confirmCardPayment(
+            paymentSecret
+          )
+          if (error) {
+            // The payment failed -- ask your customer for a new payment method.
+            console.log(error)
+          } else {
+            // The payment has succeeded.
+            console.log(paymentIntent)
+          }
+        }
+      })
+
       pr.canMakePayment().then(() => setPaymentRequest(pr))
     }
-  }, [stripe])
+  }, [stripe, paymentSecret])
 
   useEffect(() => {
-    fetch("https://yorkoratory.com/.netlify/functions/createPaymentIntent")
+    fetch("/.netlify/functions/createPaymentIntent")
       .then(r => r.text())
       .then(secret => setPaymentSecret(secret))
   }, [])
@@ -35,37 +66,6 @@ const CheckoutForm = () => {
   }, [paymentRequest, paymentSecret])
 
   if (loading) return <div>Payment not ready</div>
-  console.log(paymentRequest)
-  paymentRequest.on("paymentmethod", async ev => {
-    // Confirm the PaymentIntent without handling potential next actions (yet).
-    const { error: confirmError } = await stripe.confirmCardPayment(
-      paymentSecret,
-      { payment_method: ev.paymentMethod.id },
-      { handleActions: false }
-    )
-
-    if (confirmError) {
-      // Report to the browser that the payment failed, prompting it to
-      // re-show the payment interface, or show an error message and close
-      // the payment interface.
-      ev.complete("fail")
-    } else {
-      // Report to the browser that the confirmation was successful, prompting
-      // it to close the browser payment method collection interface.
-      ev.complete("success")
-      // Let Stripe.js handle the rest of the payment flow.
-      const { error, paymentIntent } = await stripe.confirmCardPayment(
-        paymentSecret
-      )
-      if (error) {
-        // The payment failed -- ask your customer for a new payment method.
-        console.log(error)
-      } else {
-        // The payment has succeeded.
-        console.log(paymentIntent)
-      }
-    }
-  })
   return <PaymentRequestButtonElement options={{ paymentRequest }} />
 }
 
