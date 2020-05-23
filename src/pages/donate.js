a36fd20ec12c4fa720fe43c7027b3973ff209a15
Joe <gitkraken@josephabell.co.uk>
2020-05-23 15:38:17 +0100

    update key

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import CheckoutForm from "../components/checkoutForm.js"
 
-const stripePromise = loadStripe("pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG")
+const stripePromise = loadStripe("sk_test_6sv9ejFmdNPctBPizKicJVQ0005hgqzcNn")
 
 const Donate = () => (
   <Elements stripe={stripePromise}>
