0c702ba3cbc06957c075dc45e838a910596fd5ee
Joe <gitkraken@josephabell.co.uk>
2020-05-25 20:18:55 +0100

    add header

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,9 +1,11 @@
 import React from "react"
 import CheckoutForm from "../components/checkoutForm"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Donate = () => (
   <Layout>
+    <SEO title="Donate" />
     <CheckoutForm />
   </Layout>
 )
